96ab32cee88fcb83a4aa5e71f4835ef567df9988
Jannis Gebauer <ja.geb@me.com>
2016-04-27 09:39:30 +0200

    added comments

--- dockerfiles/swift/autoreload.sh
+++ dockerfiles/swift/autoreload.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+
 start () {
+  # starts the build process in debug configuration and launches the binary in detached mode
   swift build -Xcc -fblocks -Xlinker -ldispatch -Xcc -I/usr/local/include/libbson-1.0/
   /app/.build/debug/#PROJECT_NAME# &
   PID=$!
@@ -7,6 +9,7 @@ start () {
 
 start
 
+# listens for code changes in /app/Sources and reruns start() whenever there is a change detected
 inotifywait -mr /app/Sources --format '%e %f' \
   -e modify -e delete -e move -e create -e attrib \
   --exclude '~.swift' \
